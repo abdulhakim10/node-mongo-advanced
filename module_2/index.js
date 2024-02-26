2dbacaef0c59c2de5c5e733d1b1e3aac61000c2e
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-02-26 17:34:15 +0800

    req.url, res.writeHead and res.write practice

--- module_2/index.js
+++ module_2/index.js
@@ -6,15 +6,29 @@ const _= require('underscore');
 
 const PORT = 3000;
 
+// const server = http.createServer((req, res) => {
+//     var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+// const result = _.pluck(stooges, 'age');
+
+// console.log(result);
+
+//     res.end("Hello");
+//     console.log('running......')
+// });
+
 const server = http.createServer((req, res) => {
-    var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
-const result = _.pluck(stooges, 'age');
-
-console.log(result);
-
-    res.end("Hello");
-    console.log('running......')
-});
+    if (req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Hello World</h1>');
+        res.end();
+    }
+    else if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({course:' ACC'}));
+        res.end();
+    }
+    console.log(req.url);
+})
 
 server.listen(PORT);
 console.log(`server is running at port ${PORT}`);
