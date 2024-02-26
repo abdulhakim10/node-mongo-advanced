b2c0f6a7cf4ec7063870cc68de92cb029bc26f57
abdulhakim10 <s.abdulhakim10@gmail.com>
2024-02-26 17:47:50 +0800

    url.parse practice

--- module_2/index.js
+++ module_2/index.js
@@ -1,5 +1,6 @@
 // core module
 const http = require('http');
+const url = require('url');
 
 // third party module underscore
 const _= require('underscore');
@@ -16,20 +17,28 @@ const PORT = 3000;
 //     console.log('running......')
 // });
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<h1>Hello World</h1>');
-        res.end();
-    }
-    else if (req.url === '/about'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify({course:' ACC'}));
-        res.end();
-    }
-    console.log(req.url);
-})
 
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/'){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write('<h1>Hello World</h1>');
+//         res.end();
+//     }
+//     else if (req.url === '/about'){
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         res.write(JSON.stringify({course:' ACC'}));
+//         res.end();
+//     }
+//     console.log(req.url);
+// })
+
+// url parse
+const server = http.createServer((req, res) => {
+    const address_url = 'http://localhost:3000/about?name=Tusar&country=Bangladesh';
+    const parsed_url = url.parse(address_url, true);
+    const query = parsed_url.query;
+    console.log(query);
+})
 server.listen(PORT);
 console.log(`server is running at port ${PORT}`);
 
